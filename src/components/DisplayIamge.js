a994e080bf25c99ad2d53e24c5eb14ca8b77d09a
ahmed nasser <ahmednas197@gmail.com>
2022-03-21 21:18:58 +0200

    solve issue with counter of the selectboxes of the images

--- src/components/DisplayIamge.js
+++ src/components/DisplayIamge.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../App.css'
 import HomePage from "./HomePage";
+import Checkbox from './Checkbox';
 const UploadAndDisplayImage = () => {
     const [selectedImages, setSelectedImages] = useState([]);
-    const [selectedBoxs, setSelectedBoxs] = useState([])
-
+    const [selectedBoxes, setSelectedBoxes] = useState(0)
+    const [checkedImages, setCheckedImaged] = useState([false])
 
     const imageHandelChange = (e) => {
         //console.log(e.target.files)
         if (e.target.files) {
             const fileArray = Array.from(e.target.files).map((file) => URL.createObjectURL(file))
-            console.log(fileArray)
+
             setSelectedImages((prevImages) => prevImages.concat(fileArray))
             Array.from(e.target.files).map(
                 (file) => URL.revokeObjectURL(file)
@@ -18,31 +19,27 @@ const UploadAndDisplayImage = () => {
         }
     }
 
-    const selectedBox = () => {
-        setSelectedBoxs(selectedBoxs + 1)
-    }
-
     const renderImages = (s) => {
 
         return s.map((photo) => {
-            return <>
+            return (
 
-
-                <div className="images-border col-md-3">
+                <div key={photo} className="images-border col-md-3">
                     <div className="custom-control custom-checkbox image-checkbox">
-                        <input type="checkbox" className="custom-control-input" onChange={selectedBox} id={photo} />
-                        <label className="custom-control-label" for={photo}>
+                        <input type="checkbox" className="custom-control-input" onChange={() => selectedBox()} id={photo} />
+                        <label className="custom-control-label" htmlFor={photo}>
                             <img src={photo} key={photo} alt="#" className="img-fluid" />
                         </label>
                     </div>
 
                 </div>
-
-
-
-            </>
+            )
         })
     }
+
+
+
+
 
     ///drag and drop images 
 
@@ -60,11 +57,8 @@ const UploadAndDisplayImage = () => {
     const fileDrop = (e) => {
         e.preventDefault();
         const files = e.dataTransfer.files;
-        console.log(files)
-
         if (files) {
             const fileArray = Array.from(files).map((file) => URL.createObjectURL(file))
-            console.log(fileArray)
             setSelectedImages((prevImages) => prevImages.concat(fileArray))
             Array.from(files).map(
                 (file) => URL.revokeObjectURL(file)
@@ -74,11 +68,52 @@ const UploadAndDisplayImage = () => {
     }
 
 
+    useEffect(() => {
+
+        setCheckedImaged(Array(selectedImages.length).fill(false))
+        console.log(checkedImages)
+    }, [selectedImages]);
+    //count number of selected images 
+    const selectedBox = () => {
+        // setCheckedImaged(!checkedImages)
+
+        // console.log(checkedImages)
+
+        // checkedImages.map((e) => {
+        //     console.log(e)
+        // })
+        // if (checkedImages === true) {
+        //     setSelectedBoxes(selectedBoxes + 1)
+        // }
+        // else
+        //     setSelectedBoxes(selectedBoxes - 1)
+
+        // console.log(checkedImages)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         (selectedImages.length) == 0 ?
 
             <>
-                <h5>Media </h5>
+                <p>Media </p>
                 <div className="result">
                     {renderImages(selectedImages)}
                 </div>
@@ -93,7 +128,7 @@ const UploadAndDisplayImage = () => {
 
                         <div>
                             {/*arrow svg */}
-                            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
                             </svg>
                         </div>
@@ -114,7 +149,7 @@ const UploadAndDisplayImage = () => {
             :
             <>
 
-                <h1>{selectedBoxs.length} --- {selectedImages.length} media selected </h1>
+                <p>{selectedBoxes} --- {selectedImages.length} media selected </p>
 
                 <div className="container">
 
